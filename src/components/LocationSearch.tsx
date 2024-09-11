fded2381dad7fd21bb1586628c0b17bea0466c72
Noskinpain <ebukasam108@gmail.com>
2024-09-11 12:37:15 +0100

    updated

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -1,9 +1,53 @@
-import type { Place } from "../api/place"
+import type { Place } from "../api/place";
+import { Fragment, useState } from "react";
+import { search } from "../api/search";
 
-const LocationSearch = () => {
-  return (
-    <div>LocationSearch</div>
-  )
+interface LocationSearchProps {
+  onPlaceClick: (place: Place) => void;
 }
+const LocationSearch = ({ onPlaceClick }: LocationSearchProps) => {
+  const [term, setTerm] = useState("");
+  const [places, setPlaces] = useState<Place[]>([]);
 
-export default LocationSearch
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const result = await search(term);
+    setPlaces(result);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label className="font-bold" htmlFor="term">
+          Search
+        </label>
+
+        <input
+          id="term"
+          className="border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-48"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
+
+        <h1 className="font-bold mt-6">Found Locations</h1>
+        <div className="grid grid-cols-[1fr_40px] gap-2 mt-2 items-center">
+          {places.map((place) => {
+            return (
+              <Fragment key={place.id}>
+                <p className="text-sm">{place.name}</p>
+                <button
+                  onClick={() => onPlaceClick(place)}
+                  className="bg-blue-500 text-xs text-white font-bold py-1 px-1 rounded"
+                >
+                  Go
+                </button>
+                <div className="border-b w-full col-span-2" />
+              </Fragment>
+            );
+          })}
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LocationSearch;
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,9 +1,33 @@
-import type { Place } from "../api/place"
+import "leaflet/dist/leaflet.css";
+import { Map as LeafletMap } from "leaflet";
+import { useEffect, useRef } from "react";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import type { Place } from "../api/place";
 
-const Map = () => {
-  return (
-    <div>Map</div>
-  )
+interface MapProps {
+  place: Place | null;
 }
 
-export default Map
+const Map = ({ place }: MapProps) => {
+  const mapRef = useRef<LeafletMap | null>(null);
+
+  useEffect(() => {
+    if (mapRef.current && place) {
+      mapRef.current.flyTo([place.latitude, place.longitude]);
+    }
+  }, [place]);
+  return (
+    <MapContainer
+      ref={mapRef}
+      center={[40.7, -74]}
+      zoom={12}
+      scrollWheelZoom
+      className="h-full"
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {place && <Marker position={[place?.latitude, place?.longitude]} />}
+    </MapContainer>
+  );
+};
+
+export default Map;
